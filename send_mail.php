10782697973fdbd6f4fc98bf785d770dd4df4577
manishraju1996 <manishraju1996@gmail.com>
2017-12-24 01:28:30 +0530

    delete Mailfile

--- send_mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $Email = $_POST['email'];
-   $Name = $_POST['name'];
-   $Msg = "Name = $Name\tEmail = $Email\n"
-   $myfile = fopen("guest.html", "a") or die("Unable to open file!");
-   fwrite($myfile, $Msg);
-   fclose($myfile);
- ?>
